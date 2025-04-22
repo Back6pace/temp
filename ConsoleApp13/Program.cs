d03475f5bba4439ba524297c4dc34c5711939eef
Grigoriy <145204749+Back6pace@users.noreply.github.com>
2025-04-22 22:17:30 +0300

    Update Program.cs

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -11,18 +11,18 @@ class Program
     static void Main()
     {
         
-        int myInt1 = 15614;
-        int myInt2 = 15435;
+        int myInt1 = 11714;
+        int myInt2 = 11411;
 
        
         float myFloat = 123.456789f;
 
        
-        myFloat = Math.Round(myFloat, 4);
+        myFloat = Math.Round(myFloat, 2);
 
         
         Console.WriteLine($"{"myInt1:",-10}{myInt1}");
         Console.WriteLine($"{"myInt2:",-10}{myInt2}");
-        Console.WriteLine($"{"myFloat:",-10}{myFloat:F4}");
+        Console.WriteLine($"{"myFloat:",-10}{myFloat:F2}");
     }
 }
